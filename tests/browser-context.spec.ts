1706e05f810e45772ca4c35b9a99b0c42ca3220a
karthik9629846380@gmail.com <karthik9629846380@gmail.com>
2025-11-30 10:16:34 +0530

    Browser contxt example

---
+++ tests/browser-context.spec.ts
@@ -0,0 +1,25 @@
+import{test} from '@playwright/test';
+
+
+
+test('Browser context example',async({browser})=>{
+//Browser context
+
+
+const context = await browser.newContext();
+const page =await context.newPage();
+await page.goto('https://www.amazon.in/');
+
+//Another context
+const anotherContext = await browser.newContext();
+const pageAnotehr =await context.newPage();
+await pageAnotehr.goto('https://www.google.in/');
+  
+})
+
+test('Page  example',async({page})=>{
+//Skip browser context by using page
+await page.goto('https://www.google.in/');
+  
+})
+
