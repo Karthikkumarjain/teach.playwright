64138eb14e1593f59ad5f57c50b298ea885f0b27
karthik9629846380@gmail.com <karthik9629846380@gmail.com>
2025-11-30 11:34:19 +0530

    checkbox and assertions

--- tests/checkbox.spec.ts
+++ tests/checkbox.spec.ts
@@ -1,13 +1,14 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-
+//https://playwright.dev/docs/test-assertions
 test('Test checkbox @Checkbox', async ({ page }) => {
     await page.goto('https://demoqa.com/checkbox');
 
     await page.locator('.rct-checkbox').check();
-    await page.locator('.rct-checkbox').click();
+    const expectedText = await page.locator('text=You have selected :');
 
-   await page.waitForTimeout(3000);
+    await expect(expectedText).toContainText('You have selectedsadjndbajbnd :');
+    await page.waitForTimeout(3000);
 
 
 });
